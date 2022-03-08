b1553691498a8b4df60219686d84595fa26b8154
tai1203 <cytaico@gmail.com>
2022-03-08 17:57:02 +0800

    animal

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ import requests
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('token')
-handler = WebhookHandler('secret')
+line_bot_api = LineBotApi('WMOsOUdoAEM79qwlduBXjShhmd8E/ixUZjbGgJtCxqXhepDqsG1elIPbxjEn+d5ia4YDCOIpzDdsZ12HrSJcEncYJTah+tZ5R/ZGvkq7xTLVEWUwUh8I0w+cQdEgBi/3axyU8BUFbZ+Oh6q+SB9ExQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('f4b094215349f74943f7fa4c5c7e8fea')
 
 
 @app.route("/callback", methods=['POST'])
@@ -49,27 +49,46 @@ def random_cat_url():
     print(cat_url)
     return cat_url
 
+def random_dog_url():
+    r = requests.get("https://api.thedogapi.com/v1/images/search", allow_redirects=True)
+    print(f"random_dog_url: {r.status_code}")
+
+    json = r.json()
+    dog_url = json[0]["url"]
+    print(type(dog_url))
+    print(dog_url)
+    return dog_url
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     cat_url = random_cat_url()
+    dog_url = random_dog_url()
     message = event.message.text
     if message == '喵':
         try:
             line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url= cat_url, preview_image_url= cat_url))
         except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text= 'error'))    
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text= 'error'))
+    elif message == '汪':
+        try:
+            line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url= dog_url, preview_image_url= dog_url))
+        except:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text= 'error'))
+    elif message == 'help':
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text= '需要貓貓時請說喵 要狗狗時請說汪'))
+            
 
 @handler.add(FollowEvent)
 def handle_follow_event(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text= '嗨嗨尼好 需要貓貓時請說喵')
+        TextSendMessage(text= '嗨嗨尼好 需要貓貓時請說喵 要狗狗時請說汪')
     )
 @handler.add(JoinEvent)
 def handle_join_event(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text= '嗨嗨尼好 需要貓貓時請說喵')
+        TextSendMessage(text= '嗨嗨尼好 需要貓貓時請說喵 要狗狗時請說汪')
     )
     
 if __name__ == "__main__":
